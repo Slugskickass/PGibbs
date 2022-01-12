9961d5cdc21f95d6d6f873d667878e38f82fe474
Slugskickass <a.cadby@sheffield.ac.uk>
2022-01-12 22:27:48 +0000

    Started to change the variable names and funtions to make it more readable,

--- walkthrough.py
+++ walkthrough.py
@@ -11,18 +11,19 @@ class GibbsData:
         self.P = np.empty((eff, K))
         self.N = np.empty((n, K))
 
-def updatemean(x, cl, m, p):
+
+def updatemean(dataDistro, cl, m, p):
     """
     This function update the mean of the current data
-    :param x:
-    :param cl:
-    :param m:
+    :param x:  The data associated with a specific distibution
+    :param cl: Lambda, the precision
+    :param m:  The mean of the
     :param p:
-    :return:
+    :return:  Return the mean of the data, from a distribution
     """
-    n = np.size(x)
-    xb = np.mean(x)
-    ps = n * cl + p
+    n = np.size(dataDistro)
+    xb = np.mean(dataDistro)  # The actual mean of the data
+    ps = n * cl + p  # Number of points * lambda (width) + mean
     ms = (n * cl * xb + p * m) / ps
     mean_answer = ms + np.random.normal(0, 1) / np.sqrt(ps)
     array_sum = np.sum(mean_answer)
@@ -37,13 +38,22 @@ def updatemean(x, cl, m, p):
         print(m, 'm')
     return mean_answer
 
+
 def updatelambda(x, cm, a, b):
-    ash = np.size(x) / 2 + a
-    bs = b + sum((x - cm)**2) / 2
-    salc = np.random.gamma(ash, 1 / bs)
+    """
+    :param x: The data associated with a specific distribution
+    :param cm: Current mean for this data associated with distribution
+    :param a: The prior precision mean
+    :param b: The prior precision variance
+    :return: the precision on the mean (?)
+    """
+    dataLength = np.size(x) / 2 + a
+    bs = b + sum((x - cm) ** 2) / 2
+    salc = np.random.gamma(dataLength, 1 / bs)
     return salc
 
-def updp(x, P, mu, la):
+
+def UpdateProbability(x, P, mu, la):
     K = np.size(P)
     n = np.size(x)
     salc = np.empty([n, K])
@@ -51,10 +61,11 @@ def updp(x, P, mu, la):
     for k in range(K):
         salc[:, k] = P[k] * norm.pdf(x, mu[k], 1 / np.sqrt(la[k]))
 
-    mar = np.outer(np.sum(salc, axis=1), * np.ones([1, K]))
+    mar = np.outer(np.sum(salc, axis=1), *np.ones([1, K]))
     salc = salc / mar
 
     return salc
+
 
 def newaloc(pij):
     S = np.zeros(np.shape(pij))
@@ -68,28 +79,31 @@ def newaloc(pij):
     _, cal = np.where(S == 1)
     return cal, S, nj
 
+
 def newP(nj, a):
     ash = nj + a
     sal = np.random.gamma(ash, 1)
     sal = sal / np.sum(sal)
     return sal
 
+
 M = 10000  # Number of interations
 bi = 0  # Burn in period (currently not used)
 th = 1  # Number of times between storing the data
 K = 3  # The number of distributions
 pmean = [0, 0.1]  # The prior mean, unchanged
-plas = [2, 0.6]   # The prior precision, unchanged
+plas = [2, 0.6]  # The prior precision, unchanged
 pps = [1 / 2, 1 / 2, 1 / 2]
 
 x = np.genfromtxt('forMiguel.csv')  # Loads in the test data
-np.random.shuffle(x)
-n = np.size(x)
-eff = np.ceil((M - bi) / th)
+np.random.shuffle(x)  # Shuffles the data (not very useful)
+n = np.size(x)  # Number of data points
+eff = np.ceil((M - bi) / th)  # Number of measurements to save
+sal = GibbsData(M, bi, th, K, n)  # Build empty data class of type GibbsData
 
-sal = GibbsData(M, bi, th, K, n)
+nr = np.linalg.matrix_rank(pmean)  # The rank of the matrix, this is used so that the mean and
+# lambda can be given for one distribution but used on all of them
 
-nr = np.linalg.matrix_rank(pmean)
 if nr == 1:
     pmean = np.ones((K, 2)) * pmean
 
@@ -97,9 +111,9 @@ nr = np.linalg.matrix_rank(plas)
 if nr == 1:
     plas = np.ones((K, 2)) * plas
 
-cm = np.linspace(max(x), min(x), K)  # Current mean, this holds the currect estimation of the mean for each distribution
+cm = np.linspace(max(x), min(x), K)  # Current mean, this holds the current estimation of the mean for each distribution
 
-cl = np.linspace(1, 10, K)
+cl = np.linspace(1, 10, K)           # Current lambda (precision), this holds the current estimation of the lambda for each distribution
 
 cP = np.ones((K, 1)) / K
 cP = cP.flatten()
@@ -112,23 +126,23 @@ j = 0
 for i in range(M):
     for k in range(K):
         xk = x[np.where(ca == k)]
-        cm[k] = updatemean(xk, cl[k], pmean[k, 0], pmean[k, 1])
+        cm[k] = updatemean(xk, cl[k], pmean[k, 0], pmean[k, 1])  # Returns the mean value for the distribution
         cl[k] = updatelambda(xk, cm[k], plas[k, 0], plas[k, 1])
-    cpij = updp(x, cP, cm, cl)
+    cpij = UpdateProbability(x, cP, cm, cl)
 
     ca, cS, cnj = newaloc(cpij)
     cP = newP(cnj, pps)
 
     if i > bi:
         if not np.mod(i, th):
-       #     sal.N[j] = sal.N + cS
+            #     sal.N[j] = sal.N + cS
             sal.P[j, :] = cP
             sal.lam[j, :] = cl
             sal.mu[j, :] = cm
             j = j + 1
     if not np.mod(j, 1000) and j > 1:
-        print(int(np.floor(100*(j/M))))
-        plt.plot(sal.mu[:M-(M-j)])
+        print(int(np.floor(100 * (j / M))))
+        plt.plot(sal.mu[:M - (M - j)])
         plt.show()
 
 plt.plot(sal.lam[:-2])
